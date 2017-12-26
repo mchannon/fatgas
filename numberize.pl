5d62a6366944cc9683d72a1d8f6fa200e3bd1f7f
Matt Channon <mchannon@github.com>
2017-12-26 14:44:10 -0700

    ToC numberizing tool included

---
+++ numberize.pl
@@ -0,0 +1,92 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+my $offset = 13;
+
+
+foreach( @ARGV ){
+
+
+print "file: $_\n";
+open(my $file, "<", $_) || die "File not found";
+my @lines = <$file>;
+close($file);
+
+my @sps = split( '<span class=\"ff2', $lines[ 0 ] );
+
+foreach (@sps) {
+
+	my @erg = split( '>', $_ );
+   	print "lin: $erg[-1]\n";
+}
+
+my @indices;
+my $char = '<span class="ff2';
+my $offset = 0;
+my $result = index($lines[0],$char,$offset);
+print ("\n");
+  while ($result != -1) {
+
+#    print "Found $char at $result\n";
+	push @indices, $result - 10;
+
+    $offset = $result + 1;
+    $result = index($lines[0], $char, $offset);
+
+  }
+
+print "Indices: @indices\n";
+
+my @newIndices;
+my @newValues;
+my @oldLengths;
+
+foreach (@indices) {
+	my $xn = substr( $lines[0], $_, 10 );
+#	print $xn;
+#	print(" ");
+	my $xr = substr( $lines[0], $_, 10 );
+	$xr =~ s/[^0-9]//g;
+	my $ni = index( $xn, $xr );
+#	print ("$xr ");
+#	print ( index( $xn, $xr ) );
+	push( @newIndices, $_ + $ni );
+#	print (" $_ \n");
+}
+
+#print (@newIndices);
+
+
+foreach (@newIndices)
+{
+	print ( "$_ " );
+	my $xn = substr( $lines[0], $_, 10 );
+	print ( "$xn " );
+	$xn =~ s/[^0-9]//g;
+	print $xn;
+	print(" ");
+	print length $xn;
+	print("L ");
+	push( @oldLengths, length $xn );
+	print $xn + 13;
+	print(" ");
+	$xn = lc(sprintf( "<a href=\"index.html#pf%X\">%d</a>", $xn + 13, $xn ));
+	print $xn;
+	push( @newValues, $xn );
+	print ("\n");
+}
+
+my @rIndices = reverse( @newIndices );
+my @rLengths = reverse( @oldLengths );
+my @rValues = reverse( @newValues );
+
+for (my $i = 0; $i < scalar( @rIndices ); $i++) {
+	substr( $lines[0], $rIndices[ $i ], $rLengths[ $i ] ) = $rValues[ $i ];
+}
+
+print $lines[0];
+
+}
+
