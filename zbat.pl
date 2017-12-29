1e1268720bb27a0d067b689057070ae1c7c5b597
Matt Channon <mchannon@github.com>
2017-12-29 13:38:34 -0700

    streamlining

--- zbat.pl
+++ zbat.pl
@@ -6,8 +6,21 @@ use warnings;
 system "pdf2htmlEX /Users/jrrr/Desktop/index.pdf  --zoom 2 --embed cfi --split-pages 1 -l 147";
 
 my @files = glob("*.page");
-foreach my $file(@files) {
+foreach (@files) {
 
+	open(my $file, "<", $_) || die "File not found";
+	my @lines = <$file>;
+	close($file);
+
+	my @newlines;
+	foreach( @lines ) {
+		$_ =~ s/NON                          ª/NON                      <A HREF=\"index.html#pf3\">ToC<\/A>/g;
+		push (@newlines, $_ );
+	}
+
+	open( my $file, ">", $_) || die "File no found";
+	print $file @newlines;
+	close( $file );
 }
 
 #system "sed -i '' 's/NON                          ª/NON                      <A HREF=\"index.html#pf3\">ToC<\/A>/' *.page";
@@ -24,4 +37,7 @@ system "perl numberize.pl index9.page";
 system "perl numberize.pl index10.page";
 system "perl numberize.pl index11.page";
 system "perl numberize.pl index12.page";
+system "perl numberize.pl index13.page";
+system "perl numberize.pl index14.page";
+system "perl numberize.pl index15.page";
 
