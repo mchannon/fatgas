ad87a5b622cf4552059d2f70d66cef90b3ee1c7d
Matt Channon <mchannon@github.com>
2017-12-31 14:01:41 -0700

    try some new ad stuff

--- addads.pl
+++ addads.pl
@@ -3,17 +3,19 @@
 use strict;
 use warnings;
 
+my ($adSize, $randAd, $adFiles);
+#print $adSize . $randAd . $adFiles;
 
 foreach( @ARGV ){
 
 my @fileno = split( 'index', $_ );
 my @fileno2 = split( '.page', $fileno[ 1 ] );
+my $isEven = ($fileno2[ 0 ] % 2 == 0);
 
 print "file: $_\n";
 open(my $file, "<", $_) || die "File not found";
 my @lines = <$file>;
 close($file);
-
 
 my $count = 0;
 while ( $lines[0] =~ /<\/div>/g) { $count++ }
@@ -35,31 +37,45 @@ push( @out, $sps[ 0 ] );
 #insert ad code
 push( @out, "<table bgcolor=\"#939393\" border=\"0\" " );
 
-if ( $fileno2[ 0 ] % 2 == 0 ) {
+if ( $isEven ) {
+#if ( $fileno2[ 0 ] % 2 == 0 ) {
 push( @out, "align=\"right\" " );
 }
 push( @out, " style=\"margin: 0px -3px 0px -5px\" cellpadding=\"0\" cellspacing=\"0\"><tr><td>" );
+
+my $ad1 = ad( "160x600" );
 push( @out, "<a href=\"http://www.dpbolvw.net/click-8524921-12588112\" target=\"_top\">" );
 push( @out, "<img src=\"http://www.ftjcfx.com/image-8524921-12588112\" width=\"160\" height=\"600\" alt=\"\" border=\"0\" ");
-if ( $fileno2[ 0 ] % 2 == 0 ) {
+
+if ( $isEven ) {
+#if ( $fileno2[ 0 ] % 2 == 0 ) {
 push( @out, "align=\"right\" " );
 } else {
 push( @out, "style=\"margin: -4px 0px\" " );
 }
+
+
+my $ad2 = ad( "120x600" );
+
 push( @out, "></a><a href=\"http://www.kqzyfj.com/click-8524921-10833132\" target=\"_top\"><img src=\"http://www.ftjcfx.com/image-8524921-10833132\" width=\"160\" height=\"600\" alt=\"\" border=\"0\" " );
-if ( $fileno2[ 0 ] % 2 == 0 ) {
+
+if ( $isEven ) {
+#if ( $fileno2[ 0 ] % 2 == 0 ) {
 push( @out, "align=\"right\" " );
 } else {
 push( @out, "style=\"margin: -4px 0px\" " );
 }
 push( @out, "\n/></a></td></tr><tr><td>" );
+my $ad3 = ad( "300x250" );
 
-open( my $ad, '<', 'ca4a.160x600' );
+open( my $ad, '<', 'amaz.160x600' );
 my $adData = <$ad>;
 chomp( $adData );
 chop( $adData );
 my @adchunks;
-if ( $fileno2[ 0 ] % 2 == 0 ) {
+
+if ( $isEven ) {
+#if ( $fileno2[ 0 ] % 2 == 0 ) {
 	my @adchunks = split( 'width=', $adData );
 	my $firstchunk = shift @adchunks;
 	$adData = join( '', $firstchunk, 'align="right"\n ', 'width=', join( 'width=', @adchunks ) );
@@ -71,12 +87,18 @@ close( $ad );
 
 #push( @out, "<a href=\"http://www.dpbolvw.net/click-8524921-10833150\" target=\"_top\">" );
 #push( @out, "<img src=\"http://www.lduhtrp.net/image-8524921-10833150\" width=\"160\" height=\"600\" alt=\"\" border=\"0\"" );
-if ( $fileno2[ 0 ] % 2 == 0 ) {
+if ( $isEven ) {
+#if ( $fileno2[ 0 ] % 2 == 0 ) {
 #push( @out, "align=\"right\" " );
 }
 #push( @out, "\n/></a>" );
+
+my $ad4 = ad( "300x600" );
+
+
 push( @out, "<a href=\"http://www.tkqlhce.com/click-8524921-10829166\" target=\"_top\"><img src=\"http://www.awltovhc.com/image-8524921-10829166\" width=\"120\" height=\"600\" alt=\"Get results and increase sales with GetResponse\" border=\"0\" ");
-if ( $fileno2[ 0 ] % 2 == 0 ) {
+if ( $isEven ) {
+#if ( $fileno2[ 0 ] % 2 == 0 ) {
 push( @out, "align=\"right\" " );
 }
 push( @out, "/></a></td></tr></table>" );
@@ -96,3 +118,17 @@ close $fileh;
 
 }
 
+
+sub ad {
+	($adSize) = @_;
+	print "Ad Size: $adSize, ";
+	
+	$adFiles = (<*.$adSize>);
+#	local $randno = (rand @adFiles);
+#	$randAd = $adFiles[rand @adFiles];
+
+#	print "Ad File: $randAd\n";
+
+#	return $randAd;
+	return "";
+}
